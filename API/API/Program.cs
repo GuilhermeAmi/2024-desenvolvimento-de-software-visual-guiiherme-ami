8c8b42d9f8e706803454aee2fe753c461191816d
Felipe Schafranski <felpz.gg@gmail.com>
2024-09-02 21:39:10 -0300

    Commit - Method GET

--- API/API/Program.cs
+++ API/API/Program.cs
@@ -1,6 +1,6 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.MapGet("/", () => "Deus é pai não é padrasto");
 
 app.Run();

33fd7f2877db3d577e77d55a0868dd16807c5824
Onetonio <antonioneto3260@gmail.com>
2024-10-28 21:18:23 -0300

    Commit Primeiros Componentes Criados

---
+++ my-app/src/ComponenteExemplo.tsx
@@ -0,0 +1,27 @@
+/**  ------>     
+ * Componente DEVE retornar APENAS UM elemento HTML
+ * Exportar Componente
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * <---- */
+
+
+function ComponenteExempl(){
+    return (
+        
+        <div >
+            <h1>
+                Titulo 
+            </h1>
+            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Veritatis officia mollitia excepturi corporis reiciendis aliquam quod unde debitis maxime consequuntur reprehenderit corrupti, quidem laboriosam tempore. Repellendus dolore quod consequuntur praesentium.</p>
+        </div>
+    );
+} 
+
+
+export default ComponenteExempl;
---
+++ my-app/src/ConsultaTeste.tsx
@@ -0,0 +1,26 @@
+/**  ------>     
+ * Componente DEVE retornar APENAS UM elemento HTML
+ * Exportar Componente
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * <---- */
+
+
+function ConsultaCEP(){
+    return (
+        
+        <div >
+            <h1>
+                Consulta CEP
+            </h1>
+        </div>
+    );
+} 
+
+
+export default ConsultaCEP;
